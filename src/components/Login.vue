9ab8a056d0ca7c872bb7e198a37bdbd42238d87e
AgentMrBig <agentmrbig@gmail.com>
2020-09-28 23:42:58 -0400

    added chatlist.vue and chatroom.vue, user can see a list of created chat rooms and click them to enter the chat room

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,6 +2,29 @@
   <aside>
     <h3>Sign in Anonymously</h3>
     <button @click="auth.signInAnonymously()">Sign In</button>
+
+    <div v-if="newUser">
+      <h3>Sign Up for a New Account</h3>
+      <a href="#" @click="newUser = false">Returning User?</a>
+    </div>
+
+    <label for="email">Email</label>
+    <br />
+    <input v-model="email" type="email" placeholder="email" class="input" />
+
+    <label for="password">Password</label>
+    <br />
+    <input v-model="password" type="password" class="input" />
+    <br />
+    <button
+      class="button is-info"
+      :class="{ 'is-loading': loading }"
+      @click="signInOrCreateUser()"
+    >
+      {{ newUser ? "Sign up" : "Login" }}
+    </button>
+
+    <p class="has-text-danger" v-if="errorMessage">{{ errorMessage }}</p>
   </aside>
 </template>
 
@@ -12,7 +35,27 @@ export default {
   data() {
     return {
       auth,
+      email: "",
+      password: "",
+      newUser: false,
+      errorMessage: "",
+      loading: false,
     };
+  },
+  methods: {
+    async signInOrCreateUser() {
+      this.loading = true;
+      this.errorMessage = "";
+      try {
+        if (this.newUser) {
+          await auth.createUserWithEmailAndPassword(this.email, this.password);
+        } else {
+          await auth.signInWithEmailAndPassword(this.email, this.password);
+        }
+      } catch (error) {
+        this.errorMessage = error.message;
+      }
+    },
   },
 };
 </script>
